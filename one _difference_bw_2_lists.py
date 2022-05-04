84222dfbad7a698e6663e01252584c1a4432af0f
gayathribrcm-test <60910682+gayathribrcm-test@users.noreply.github.com>
2022-05-04 10:06:34 +0530

    Update one _difference_bw_2_lists.py

--- one _difference_bw_2_lists.py
+++ one _difference_bw_2_lists.py
@@ -7,22 +7,21 @@ aabbc bbaad
 '''
 def sorted_l(l1):
     d1={}
-    for i in l1:
-        for j in i:
+    for j in l1:
             d1[j]=d1.get(j,0)+1
     return dict(sorted(d1.items(),key=lambda kv:kv[0]))
 def one_diff_list(list_1,list_2):
     '''There are two lists having string elements, \
     I wanted the elements which are having\
     only one difference to the corresponding element in another list.'''
-    for i in list_1:
-        dict_i = sorted_l(list(i))
+    for i in list_1: #iterate each element in list aabbc
+        dict_i = sorted_l(list(i)) #convert to dict {'a': 2, 'b': 2, 'c': 1}
         for j in list_2:
-            dict_j = sorted_l(list(j))
-            for k,v in dict_i.items():
+            dict_j = sorted_l(list(j)) #convert to dict {'a': 2, 'b': 2, 'd': 1}
+            for k,v in dict_i.items():#compare every key of dict i in dict j and ignore if same key,value 
                 if k in dict_j.keys() and v==dict_j[k]:
                     pass
-                elif k not in dict_j.keys() and v==1:
+                elif k not in dict_j.keys() and v==1:#else k of i is not  in dictj and  v==1, take the diff of dict i and dict j keys,if len of diff is one print 
                     l1 = list(set(i) - set(j))
                     l2 = list(set(j) - set(i))
                     if len(l1) == 1 and len(l2) == 1:
